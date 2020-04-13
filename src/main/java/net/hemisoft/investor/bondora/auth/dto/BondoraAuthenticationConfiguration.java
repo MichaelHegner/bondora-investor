aa3b659419fdab432818fdb85ee252d16b49d705
Michael Hegner <michaelhegner77@gmail.com>
2020-04-13 21:27:40 +0200

    Update BondoraAuthenticationConfiguration.java

--- src/main/java/net/hemisoft/investor/bondora/auth/dto/BondoraAuthenticationConfiguration.java
+++ src/main/java/net/hemisoft/investor/bondora/auth/dto/BondoraAuthenticationConfiguration.java
@@ -1,31 +1,21 @@
-//package net.hemisoft.investor.bondora.auth.dto;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//import org.springframework.security.oauth2.client.registration.ClientRegistration;
-//import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
-//import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
-//import org.springframework.security.oauth2.core.AuthorizationGrantType;
-//import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
-//
-//@Configuration
-//public class BondoraAuthenticationConfiguration extends WebSecurityConfigurerAdapter {
-//	@Override
-//	protected void configure(HttpSecurity http) throws Exception {
-//		http.authorizeRequests().antMatchers("/", "/login**", "/error**", "/callback**", "/user**").permitAll()
-//		        .anyRequest().authenticated()
-//		        .and().logout().logoutUrl("/logout").logoutSuccessUrl("/")
-//		        .and().oauth2Login()
-//		        .defaultSuccessUrl("/account")
-//		        .failureUrl("/error");
-//	}
-//
+package net.hemisoft.investor.bondora.auth.dto;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class BondoraAuthenticationConfiguration extends WebSecurityConfigurerAdapter {
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().antMatchers("/", "/login**", "/error**", "/callback**", "/user**").permitAll()
+		        .anyRequest().authenticated()
+		        .and().logout().logoutUrl("/logout").logoutSuccessUrl("/")
+		        .and().oauth2Login()
+		        .defaultSuccessUrl("/account")
+		        .failureUrl("/error");
+	}
+
 //	@Bean
 //	public ClientRegistrationRepository clientRegistrationRepository(
 //			@Value("${client-id}") String clientId, // Configured in system properties (Eclipse)
@@ -50,5 +40,5 @@
 //				.clientName("Bondora")
 //				.build();
 //	}
-//
-//}
+
+}
