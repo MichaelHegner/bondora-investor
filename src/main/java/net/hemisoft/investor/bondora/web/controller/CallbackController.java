ebad74449840614e88b173071080b6582c0d0cb8
Michael Hegner <michaelhegner77@gmail.com>
2020-04-13 21:43:50 +0200

    Update CallbackController.java

--- src/main/java/net/hemisoft/investor/bondora/web/controller/CallbackController.java
+++ src/main/java/net/hemisoft/investor/bondora/web/controller/CallbackController.java
@@ -1,6 +1,6 @@
 package net.hemisoft.investor.bondora.web.controller;
 
-import org.springframework.security.oauth2.core.OAuth2AuthenticatedPrincipal;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -9,7 +9,7 @@ import org.springframework.web.servlet.ModelAndView;
 public class CallbackController {
 
 	@GetMapping("/callback")
-	public ModelAndView get(String code, OAuth2AuthenticatedPrincipal token) {
+	public ModelAndView get(String code, OAuth2AuthenticationToken token) {
 		ModelAndView mav = new ModelAndView("callback");
 		mav.addObject("code", code);
 		mav.addObject("principal", token);
