245ac877221241bdee9f97dac1824c3dc01c5ed2
Michael Hegner <michaelhegner77@gmail.com>
2020-04-13 22:15:30 +0200

    Update CallbackController.java

--- src/main/java/net/hemisoft/investor/bondora/web/controller/CallbackController.java
+++ src/main/java/net/hemisoft/investor/bondora/web/controller/CallbackController.java
@@ -1,20 +1,41 @@
 package net.hemisoft.investor.bondora.web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class CallbackController {
+	
+	@Value("${client-id}")     String clientId;
+	@Value("${client-secret}") String clientSecret;
+	
+	
+	@Autowired
+	RestTemplate template;
 
 	@GetMapping("/callback")
 	public ModelAndView get(String code, OAuth2AuthenticationToken token) {
 		System.out.println("TOKEN: " + token);
 		
+		
+		MultiValueMap<String, Object> parts = new LinkedMultiValueMap<>();
+		parts.add("grant_type", "authorization_code");
+		parts.add("client_id", clientId);
+		parts.add("client_secret", clientSecret);
+		parts.add("code", code);
+//		parts.add("redirect_uri", "/callback");
+		String response = template.postForObject("https://api.bondora.com/oauth/access_token", parts, String.class);
+		
 		ModelAndView mav = new ModelAndView("callback");
 		mav.addObject("code", code);
-		mav.addObject("principal", token);
+		mav.addObject("principal", response);
 		return mav;
 	}
 	
