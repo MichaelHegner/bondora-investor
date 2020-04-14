7e14aa56ac2160a4964238cd19add7a52a2fc5ae
Michael Hegner <michaelhegner77@gmail.com>
2020-04-14 16:03:19 +0200

    Update BondoraAuthenticationConfiguration.java

--- src/main/java/net/hemisoft/investor/bondora/auth/BondoraAuthenticationConfiguration.java
+++ src/main/java/net/hemisoft/investor/bondora/auth/BondoraAuthenticationConfiguration.java
@@ -1,12 +1,14 @@
-//package net.hemisoft.investor.bondora.auth;
-//
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//
-//@Configuration
-//public class BondoraAuthenticationConfiguration extends WebSecurityConfigurerAdapter {
-//	
+package net.hemisoft.investor.bondora.auth;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+
+@Configuration
+public class BondoraAuthenticationConfiguration extends WebSecurityConfigurerAdapter {
+	
 //	@Override
 //	protected void configure(HttpSecurity http) throws Exception {
 //		http.authorizeRequests().antMatchers("/", "/login**", "/error**", "/callback**", "/user**").permitAll()
@@ -15,5 +17,21 @@
 //		        .and().oauth2Login()
 //		;
 //	}
-//
-//}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+			.authorizeRequests( 
+					a -> a.antMatchers("/", "/error**", "/webjars/**")
+						.permitAll()
+						.anyRequest()
+						.authenticated()
+		        )
+			.exceptionHandling(
+					e -> e.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+					)
+			.oauth2Login()
+		;
+	}
+
+}
