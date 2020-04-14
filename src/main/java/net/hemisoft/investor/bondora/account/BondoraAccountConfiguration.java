ded72262c3699f0a5bccef09eff738feaf73cc91
Michael Hegner <michaelhegner77@gmail.com>
2020-04-14 17:24:10 +0200

    OAuth2

--- src/main/java/net/hemisoft/investor/bondora/account/BondoraAccountConfiguration.java
+++ src/main/java/net/hemisoft/investor/bondora/account/BondoraAccountConfiguration.java
@@ -1,39 +1,39 @@
-package net.hemisoft.investor.bondora.account;
-
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.integration.dsl.IntegrationFlow;
-import org.springframework.integration.dsl.IntegrationFlows;
-import org.springframework.integration.http.dsl.Http;
-
-import lombok.NoArgsConstructor;
-
-@Configuration
-@NoArgsConstructor
-public class BondoraAccountConfiguration {
-	
-	@Value("${app.bondora.url.account.balance}") String balanceUrl;
-
-	@Bean
-	public IntegrationFlow requestAccountFlow() {
-		 return IntegrationFlows.from(Http.inboundGateway("/account")
-		            .requestMapping(m -> m.methods(HttpMethod.GET))
-		            .requestPayloadType(String.class))
-		        .channel("httpAccountRequest")
-		        .get();
-	}
-	
-	@Bean
-	public IntegrationFlow bondoraAccountFlow() {
-		return IntegrationFlows.from("httpAccountRequest")
-				.handle(
-						Http.outboundGateway(balanceUrl)
-						.httpMethod(HttpMethod.GET)
-						.expectedResponseType(String.class)					
-						)
-				.logAndReply();
-	}
-	
-}
+//package net.hemisoft.investor.bondora.account;
+//
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.http.HttpMethod;
+//import org.springframework.integration.dsl.IntegrationFlow;
+//import org.springframework.integration.dsl.IntegrationFlows;
+//import org.springframework.integration.http.dsl.Http;
+//
+//import lombok.NoArgsConstructor;
+//
+//@Configuration
+//@NoArgsConstructor
+//public class BondoraAccountConfiguration {
+//	
+//	@Value("${app.bondora.url.account.balance}") String balanceUrl;
+//
+//	@Bean
+//	public IntegrationFlow requestAccountFlow() {
+//		 return IntegrationFlows.from(Http.inboundGateway("/account")
+//		            .requestMapping(m -> m.methods(HttpMethod.GET))
+//		            .requestPayloadType(String.class))
+//		        .channel("httpAccountRequest")
+//		        .get();
+//	}
+//	
+//	@Bean
+//	public IntegrationFlow bondoraAccountFlow() {
+//		return IntegrationFlows.from("httpAccountRequest")
+//				.handle(
+//						Http.outboundGateway(balanceUrl)
+//						.httpMethod(HttpMethod.GET)
+//						.expectedResponseType(String.class)					
+//						)
+//				.logAndReply();
+//	}
+//	
+//}
--- src/main/java/net/hemisoft/investor/bondora/auth/BondoraAuthenticationConfiguration.java
+++ src/main/java/net/hemisoft/investor/bondora/auth/BondoraAuthenticationConfiguration.java
@@ -1,11 +1,12 @@
-package net.hemisoft.investor.bondora.auth;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-@Configuration
-public class BondoraAuthenticationConfiguration extends WebSecurityConfigurerAdapter {
-	
+//package net.hemisoft.investor.bondora.auth;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//
+//@Configuration
+//public class BondoraAuthenticationConfiguration extends WebSecurityConfigurerAdapter {
+//	
 //	@Override
 //	protected void configure(HttpSecurity http) throws Exception {
 //		http.authorizeRequests().antMatchers("/", "/login**", "/login/**", "/error**", "/callback**", "/user**").permitAll()
@@ -14,21 +15,21 @@ public class BondoraAuthenticationConfiguration extends WebSecurityConfigurerAda
 //		        .and().oauth2Login()
 //		;
 //	}
-
-//	@Override
-//	protected void configure(HttpSecurity http) throws Exception {
-//		http
-//			.authorizeRequests( 
-//					a -> a.antMatchers("/", "/error**", "/webjars/**", "/favicon.ico")
-//						.permitAll()
-//						.anyRequest()
-//						.authenticated()
-//		        )
-//			.exceptionHandling(
-//					e -> e.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
-//					)
-//			.oauth2Login()
-//		;
-//	}
-
-}
+//
+////	@Override
+////	protected void configure(HttpSecurity http) throws Exception {
+////		http
+////			.authorizeRequests( 
+////					a -> a.antMatchers("/", "/error**", "/webjars/**", "/favicon.ico")
+////						.permitAll()
+////						.anyRequest()
+////						.authenticated()
+////		        )
+////			.exceptionHandling(
+////					e -> e.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+////					)
+////			.oauth2Login()
+////		;
+////	}
+//
+//}
