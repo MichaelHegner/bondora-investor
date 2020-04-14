818b98ffe1edb45471a0cd49a5264ecb0d41a67c
Michael Hegner <michaelhegner77@gmail.com>
2020-04-14 20:51:29 +0200

    OAUth2

--- src/main/java/net/hemisoft/investor/bondora/web/controller/AccountController.java
+++ src/main/java/net/hemisoft/investor/bondora/web/controller/AccountController.java
@@ -7,6 +7,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Controller
 public class AccountController {
 	@Value("${app.bondora.url.account.balance}") 
@@ -17,6 +20,9 @@ public class AccountController {
 
 	@GetMapping("/account")
 	public ModelAndView get() {
+		log.info("getAccount on " + balanceUrl);
+		template.getInterceptors().forEach(i -> log.info(i.toString()));
+		
 		ModelAndView mav = new ModelAndView("account");
 		mav.addObject("result", template.getForObject(balanceUrl, String.class));
 		return mav;
