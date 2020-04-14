56a37f3d3ca0ab8e5cf48d3d96353d7001914bd0
Michael Hegner <michaelhegner77@gmail.com>
2020-04-14 14:59:13 +0200

    Update IndexController.java

--- src/main/java/net/hemisoft/investor/bondora/web/controller/IndexController.java
+++ src/main/java/net/hemisoft/investor/bondora/web/controller/IndexController.java
@@ -1,14 +1,14 @@
-package net.hemisoft.investor.bondora.web.controller;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.servlet.view.RedirectView;
-
-@Controller
-public class IndexController {
-
-	@GetMapping
-	public RedirectView index() {
-		return new RedirectView("/login");
-	}
-}
+//package net.hemisoft.investor.bondora.web.controller;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.servlet.view.RedirectView;
+//
+//@Controller
+//public class IndexController {
+//
+//	@GetMapping
+//	public RedirectView index() {
+//		return new RedirectView("/login");
+//	}
+//}
