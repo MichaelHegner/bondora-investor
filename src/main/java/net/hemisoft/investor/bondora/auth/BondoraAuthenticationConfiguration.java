d092acb48307f0cd85e465bdf7ee92ab059f6e81
Michael Hegner <michaelhegner77@gmail.com>
2020-04-14 15:14:34 +0200

    OAUTH2

--- src/main/java/net/hemisoft/investor/bondora/auth/BondoraAuthenticationConfiguration.java
+++ src/main/java/net/hemisoft/investor/bondora/auth/BondoraAuthenticationConfiguration.java
@@ -1,19 +1,19 @@
-package net.hemisoft.investor.bondora.auth;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-@Configuration
-public class BondoraAuthenticationConfiguration extends WebSecurityConfigurerAdapter {
-	
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/", "/login**", "/error**", "/callback**", "/user**").permitAll()
-		        .anyRequest().authenticated()
-		        .and().logout().logoutUrl("/logout").logoutSuccessUrl("/")
-		        .and().oauth2Login()
-		;
-	}
-
-}
+//package net.hemisoft.investor.bondora.auth;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//
+//@Configuration
+//public class BondoraAuthenticationConfiguration extends WebSecurityConfigurerAdapter {
+//	
+//	@Override
+//	protected void configure(HttpSecurity http) throws Exception {
+//		http.authorizeRequests().antMatchers("/", "/login**", "/error**", "/callback**", "/user**").permitAll()
+//		        .anyRequest().authenticated()
+//		        .and().logout().logoutUrl("/logout").logoutSuccessUrl("/")
+//		        .and().oauth2Login()
+//		;
+//	}
+//
+//}
--- src/main/java/net/hemisoft/investor/bondora/auth/dto/AuthDto.java
+++ src/main/java/net/hemisoft/investor/bondora/auth/dto/AuthDto.java
@@ -1,15 +1,15 @@
-package net.hemisoft.investor.bondora.auth.dto;
-
-import lombok.NonNull;
-import lombok.Value;
-
-@Value
-public class AuthDto {
-	final String responseType = "code";
-	
-	@NonNull String clientId;
-	
-	String state       = "xyz";
-	String scope       = "BidsRead";
-	String redirectUri = "http:localhost:8080/account";
-}
+//package net.hemisoft.investor.bondora.auth.dto;
+//
+//import lombok.NonNull;
+//import lombok.Value;
+//
+//@Value
+//public class AuthDto {
+//	final String responseType = "code";
+//	
+//	@NonNull String clientId;
+//	
+//	String state       = "xyz";
+//	String scope       = "BidsRead";
+//	String redirectUri = "http:localhost:8080/account";
+//}
