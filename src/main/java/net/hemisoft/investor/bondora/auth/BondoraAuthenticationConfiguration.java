565ae6ae421337b5ef4eddec3bc9d71299318b99
Michael Hegner <michaelhegner77@gmail.com>
2020-04-14 17:53:27 +0200

    Update BondoraAuthenticationConfiguration.java

--- src/main/java/net/hemisoft/investor/bondora/auth/BondoraAuthenticationConfiguration.java
+++ src/main/java/net/hemisoft/investor/bondora/auth/BondoraAuthenticationConfiguration.java
@@ -1,35 +1,35 @@
-//package net.hemisoft.investor.bondora.auth;
-//
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//
-//@Configuration
-//public class BondoraAuthenticationConfiguration extends WebSecurityConfigurerAdapter {
-//	
+package net.hemisoft.investor.bondora.auth;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class BondoraAuthenticationConfiguration extends WebSecurityConfigurerAdapter {
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().antMatchers("/login**", "/error**", "/callback**").permitAll()
+		        .anyRequest().authenticated()
+		        .and().logout().logoutUrl("/logout").logoutSuccessUrl("/")
+		        .and().oauth2Login()
+		;
+	}
+
 //	@Override
 //	protected void configure(HttpSecurity http) throws Exception {
-//		http.authorizeRequests().antMatchers("/", "/login**", "/login/**", "/error**", "/callback**", "/user**").permitAll()
-//		        .anyRequest().authenticated()
-//		        .and().logout().logoutUrl("/logout").logoutSuccessUrl("/")
-//		        .and().oauth2Login()
+//		http
+//			.authorizeRequests( 
+//					a -> a.antMatchers("/", "/error**", "/webjars/**", "/favicon.ico")
+//						.permitAll()
+//						.anyRequest()
+//						.authenticated()
+//		        )
+//			.exceptionHandling(
+//					e -> e.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+//					)
+//			.oauth2Login()
 //		;
 //	}
-//
-////	@Override
-////	protected void configure(HttpSecurity http) throws Exception {
-////		http
-////			.authorizeRequests( 
-////					a -> a.antMatchers("/", "/error**", "/webjars/**", "/favicon.ico")
-////						.permitAll()
-////						.anyRequest()
-////						.authenticated()
-////		        )
-////			.exceptionHandling(
-////					e -> e.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
-////					)
-////			.oauth2Login()
-////		;
-////	}
-//
-//}
+
+}
