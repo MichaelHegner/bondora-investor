0cf5a417d70a2e8f82a32a4d304ec5e9d5364d5f
Michael Hegner <michaelhegner77@gmail.com>
2020-04-14 17:37:50 +0200

    OAuth2

--- src/main/java/net/hemisoft/investor/bondora/BondoraInvestorApplication.java
+++ src/main/java/net/hemisoft/investor/bondora/BondoraInvestorApplication.java
@@ -2,6 +2,10 @@ package net.hemisoft.investor.bondora;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class BondoraInvestorApplication {
@@ -10,17 +14,17 @@ public class BondoraInvestorApplication {
 		SpringApplication.run(BondoraInvestorApplication.class, args);
 	}
 
-//	@Bean
-//	public RestTemplate restTemplate(OAuth2AuthorizedClientService clientService) {
-//		return new RestTemplateBuilder()
-//				.interceptors(
+	@Bean
+	public RestTemplate restTemplate(OAuth2AuthorizedClientService clientService) {
+		return new RestTemplateBuilder()
+				.interceptors(
 //				 (ClientHttpRequestInterceptor) (httpRequest, bytes, execution) -> {
 //					 OAuth2AuthenticationToken token = OAuth2AuthenticationToken.class.cast(SecurityContextHolder.getContext().getAuthentication());
 //					 OAuth2AuthorizedClient client = clientService.loadAuthorizedClient(token.getAuthorizedClientRegistrationId(), token.getName());
 //					 httpRequest.getHeaders().add(HttpHeaders.AUTHORIZATION, "Bearer" + client.getAccessToken().getTokenValue());
 //					 return execution.execute(httpRequest, bytes);
 //				 }
-//				)
-//				.build();
-//	}
+				)
+				.build();
+	}
 }
