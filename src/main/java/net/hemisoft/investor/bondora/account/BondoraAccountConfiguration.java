d91ef8ab688667dafa4c690faf27c0b29f75eef6
Michael Hegner <michaelhegner77@gmail.com>
2020-04-14 20:17:22 +0200

    Update BondoraAccountConfiguration.java

--- src/main/java/net/hemisoft/investor/bondora/account/BondoraAccountConfiguration.java
+++ src/main/java/net/hemisoft/investor/bondora/account/BondoraAccountConfiguration.java
@@ -1,21 +1,25 @@
 package net.hemisoft.investor.bondora.account;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.integration.dsl.IntegrationFlow;
 import org.springframework.integration.dsl.IntegrationFlows;
 import org.springframework.integration.http.dsl.Http;
 
 import lombok.NoArgsConstructor;
+import net.hemisoft.investor.bondora.web.session.AccessTokenService;
 
 @Configuration
 @NoArgsConstructor
 public class BondoraAccountConfiguration {
 	
 	@Value("${app.bondora.url.account.balance}") String balanceUrl;
-
+	
 	@Bean
 	public IntegrationFlow requestAccountFlow() {
 		 return IntegrationFlows.from(Http.inboundGateway("/account")
@@ -26,8 +30,9 @@ public class BondoraAccountConfiguration {
 	}
 	
 	@Bean
-	public IntegrationFlow bondoraAccountFlow() {
+	public IntegrationFlow bondoraAccountFlow(AccessTokenService accessTokenService) {
 		return IntegrationFlows.from("httpAccountRequest")
+				.enrichHeaders(Map.of(HttpHeaders.AUTHORIZATION, "Bearer" + accessTokenService.getAccessToken().getAccess_token()))
 				.handle(
 						Http.outboundGateway(balanceUrl)
 						.httpMethod(HttpMethod.GET)
