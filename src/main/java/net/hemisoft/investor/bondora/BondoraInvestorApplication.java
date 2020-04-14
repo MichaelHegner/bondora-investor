690146a91d65d81c16923a9951fa6bc9888f1e00
Michael Hegner <michaelhegner77@gmail.com>
2020-04-14 18:28:34 +0200

    Update BondoraInvestorApplication.java

--- src/main/java/net/hemisoft/investor/bondora/BondoraInvestorApplication.java
+++ src/main/java/net/hemisoft/investor/bondora/BondoraInvestorApplication.java
@@ -1,6 +1,5 @@
 package net.hemisoft.investor.bondora;
 
-import static org.springframework.context.annotation.ScopedProxyMode.TARGET_CLASS;
 import static org.springframework.web.context.WebApplicationContext.SCOPE_SESSION;
 
 import org.springframework.boot.SpringApplication;
@@ -8,7 +7,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.web.client.RestTemplate;
 
 import net.hemisoft.investor.bondora.web.session.AccessToken;
@@ -25,18 +30,18 @@ public class BondoraInvestorApplication {
 	public RestTemplate restTemplate(OAuth2AuthorizedClientService clientService) {
 		return new RestTemplateBuilder()
 				.interceptors(
-//				 (ClientHttpRequestInterceptor) (httpRequest, bytes, execution) -> {
-//					 OAuth2AuthenticationToken token = OAuth2AuthenticationToken.class.cast(SecurityContextHolder.getContext().getAuthentication());
-//					 OAuth2AuthorizedClient client = clientService.loadAuthorizedClient(token.getAuthorizedClientRegistrationId(), token.getName());
-//					 httpRequest.getHeaders().add(HttpHeaders.AUTHORIZATION, "Bearer" + client.getAccessToken().getTokenValue());
-//					 return execution.execute(httpRequest, bytes);
-//				 }
+				 (ClientHttpRequestInterceptor) (httpRequest, bytes, execution) -> {
+					 OAuth2AuthenticationToken token = OAuth2AuthenticationToken.class.cast(SecurityContextHolder.getContext().getAuthentication());
+					 OAuth2AuthorizedClient client = clientService.loadAuthorizedClient(token.getAuthorizedClientRegistrationId(), token.getName());
+					 httpRequest.getHeaders().add(HttpHeaders.AUTHORIZATION, "Bearer" + client.getAccessToken().getTokenValue());
+					 return execution.execute(httpRequest, bytes);
+				 }
 				)
 				.build();
 	}
 	
 	@Bean
-	@Scope(value = SCOPE_SESSION, proxyMode = TARGET_CLASS)
+	@Scope(value = SCOPE_SESSION, proxyMode = ScopedProxyMode.TARGET_CLASS)
 	public AccessToken accessToken() {
 		return new AccessToken();
 	}
